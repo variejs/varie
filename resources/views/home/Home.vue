2e6b4ca13624ca6be463de6979182acaf9721155
Luke Policinski <luke.policinski@egov.com>
2017-11-16 12:41:27 -0500

    some basic homepage

--- resources/views/home/Home.vue
+++ resources/views/home/Home.vue
@@ -1,6 +1,56 @@
+<style>
+    html, body {
+        color: #636b6f;
+        font-family: 'Montserrat', sans-serif;
+        font-weight: 100;
+        height: 100vh;
+        margin: 0;
+    }
+    .full-height {
+        height: 100vh;
+    }
+    .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+    .position-ref {
+        position: relative;
+    }
+    .content {
+        text-align: center;
+    }
+    .title {
+        font-size: 84px;
+    }
+    .links > a {
+        color: #636b6f;
+        padding: 0 25px;
+        font-size: 12px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+    .m-b-md {
+        margin-bottom: 30px;
+    }
+</style>
 <template>
 	<section>
-        Welcome to Varie!
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:300" rel="stylesheet">
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    Varie
+                </div>
+
+                <div class="links">
+                    <a href="#">Documentation</a>
+                    <a href="https://github.com/codepier/varie">GitHub</a>
+                </div>
+            </div>
+        </div>
 	</section>
 </template>
 
