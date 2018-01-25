14ec0c2a6108a02833a17a470dddfbbdfe100dfc
Luke Policinski <luke.policinski@egov.com>
2018-01-25 10:14:14 -0500

    adding https config

---
+++ config/http.ts
@@ -0,0 +1,26 @@
+export default {
+
+  /*
+  |--------------------------------------------------------------------------
+  | Base URL
+  |--------------------------------------------------------------------------
+  |
+  | Sometimes you need to setup a base url for all your requests
+  |
+  */
+  baseURL: null,
+
+  /*
+  |--------------------------------------------------------------------------
+  | Customized Headers
+  |--------------------------------------------------------------------------
+  |
+  | If you need to attach headers with every requests you should do that here
+  |
+  */
+  headers: {
+    common: {
+      "X-Requested-With": "XMLHttpRequest"
+    }
+  },
+};
