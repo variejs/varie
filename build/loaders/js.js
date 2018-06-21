1c9383a02f0f411cc517826e14b15b6f9589147d
Luke Policinski <luke@lukepolo.com>
2018-06-21 09:39:35 -0400

    using babel now

--- build/loaders/js.js
+++ build/loaders/js.js
@@ -1,10 +1,12 @@
 module.exports = function jsLoader() {
   return {
     test: /\.js$/,
-    exclude: /(node_modules)/,
-    // TODO - cant get babel loader to work properly
-    // use: {
-    //     loader: 'babel-loader',
-    // }
+      exclude: /(node_modules|bower_components)/,
+      use: {
+          loader: 'babel-loader',
+          options: {
+              presets: ['babel-preset-env']
+          }
+      }
   };
 };
