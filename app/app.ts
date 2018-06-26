1eb544527e9ad83936e626e91d6035acbfb80600
Luke Policinski <luke.policinski@egov.com>
2018-06-26 12:43:42 -0400

    Updating vendors, bumping to node LTS

--- app/app.ts
+++ app/app.ts
@@ -9,11 +9,11 @@
 import Vue from "vue";
 import App from "@views/layouts/App.vue";
 
-import { Application } from "varie";
+import { application } from "varie";
 import RouterInterface from "varie/lib/routing/RouterInterface";
 import StateServiceInterface from "varie/lib/state/StateServiceInterface";
 
-new Application().boot().then((app) => {
+application.boot().then((app) => {
   new Vue({
     store: app.make<StateServiceInterface>("$store").getStore(),
     router: app.make<RouterInterface>("$router").getRouter(),
