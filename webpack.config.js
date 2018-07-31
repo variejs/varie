c9bf66a50d19af0b9ac93a02e19f79a3e334843d
Luke Policinski <luke@lukepolo.com>
2018-07-30 22:03:13 -0400

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,25 @@
-let varieBundler = require('varie-bundler');
+const path = require('path');
+const VarieBundler = require('varie-bundler');
+
 module.exports = function(env, argv) {
- return varieBundler(env, argv, __dirname);
-};
+ return new VarieBundler(argv, __dirname)
+     .variables({
+
+     })
+     .aliases({
+         "@app": path.join(__dirname, "app"),
+         "@routes": path.join(__dirname, "routes"),
+         "@config": path.join(__dirname, "config"),
+         "@store": path.join(__dirname, "app/store"),
+         "@models": path.join(__dirname, "app/models"),
+         "@resources": path.join(__dirname, "resources"),
+         "@views": path.join(__dirname, "resources/views"),
+         "@components": path.join(__dirname, "app/components"),
+     })
+     .customWebpackConfig({
+
+     })
+     .addLoader()
+     .addPlugin()
+     .build()
+}
