c250526960a727c2990ba11da5b34a0026ac8951
Luke Policinski <luke@lukepolo.com>
2018-08-12 08:25:42 -0400

    Moving the store and views around a bit, it was getting annoying to move around the app

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,12 @@ module.exports = function(env, argv) {
     .entry("app", ["app/app.ts", "resources/sass/app.scss"])
     .aliases({
       "@app": path.join(__dirname, "app"),
-      "@routes": path.join(__dirname, "routes"),
+      "@views": path.join(__dirname, "views"),
+      "@store": path.join(__dirname, "store"),
       "@config": path.join(__dirname, "config"),
-      "@store": path.join(__dirname, "app/store"),
+      "@routes": path.join(__dirname, "routes"),
       "@models": path.join(__dirname, "app/models"),
       "@resources": path.join(__dirname, "resources"),
-      "@views": path.join(__dirname, "resources/views"),
       "@components": path.join(__dirname, "app/components"),
     })
     .build();
